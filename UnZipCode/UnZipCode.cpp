a3b870e16092ffe704adb3141a8d82d748efcb7a
AozoraANDER <145823886+AozoraANDER@users.noreply.github.com>
2024-11-12 05:17:50 -0700

    Initialize

---
+++ UnZipCode/UnZipCode.cpp
@@ -0,0 +1,96 @@
+﻿// UnZipCode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <tchar.h>
+#include <stdlib.h>
+
+struct ZipData {
+  char filename[256];
+  int filesize;
+};
+
+int _tmain(int argc, _TCHAR* argv[]) 
+{ 
+	//argv[1];
+  FILE* fp;
+  if (fopen_s(&fp, argv[1], "rb") != 0) {
+    fprintf(stderr, "Failed to open file: %s\n", argv[1]);
+    return -1;
+  }
+
+  int num;
+
+  if (fread_s(&num, sizeof(num), sizeof(int), 1, fp) !=1) {  // Read total file count
+    fprintf(stderr, "Failed to read file count\n");
+    fclose(fp);
+    return -1;
+  }
+
+  printf("%d\n", num);
+
+  struct ZipData* pInfo = (struct ZipData*)malloc(num * sizeof(struct ZipData));
+
+  if (pInfo == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    fclose(fp);
+    return -1;
+  }
+
+  if (fread_s(pInfo, num * sizeof(struct ZipData), sizeof(struct ZipData), num,
+              fp) != num) {
+    fprintf(stderr, "Failed to read file metadata\n");
+    free(pInfo);
+    fclose(fp);
+    return -1;
+  }
+
+  FILE* fpDst;
+  char buff[1024];
+  char c;
+  int r;
+
+  for (int i = 0; i < num; i++) {
+    // Check if have file has same name.
+    if (fopen_s(&fpDst, pInfo[i].filename, "r") == 0) {
+      fclose(fpDst);  // if exist, close file.
+      printf("File '%s' already exists. Overwrite? (y/n): ", pInfo[i].filename);
+      char choice;
+      std::cin >> choice;
+
+      if (choice == 'n' || choice == 'N') {
+        printf("Skipping file: %s\n", pInfo[i].filename);
+        continue;  // skip it.
+      }
+    }
+  }
+
+  for (int i = 0; i < num; i++) {
+  //Create file from the content one by one, read data from the packed file and write to new created file
+    if (fopen_s(&fpDst, pInfo[i].filename, "wb") != 0) {
+      fprintf(stderr, "Failed to create file: %s\n", pInfo[i].filename);
+      continue;  // Skip this file if failed to open
+    }
+
+	for (int j = 0; j < pInfo[i].filesize; j++) {
+
+      if (fread_s(&c, sizeof(c), 1, 1, fp) != 1) { 
+          fprintf(stderr, "Failed to read content for file: %s\n", pInfo[i].filename);
+        break;
+          }
+      if (fwrite(&c, 1, 1, fpDst) != 1) {
+            fprintf(stderr, "Failed to write content to file: %s\n",
+                pInfo[i].filename);
+        break;
+      }
+	}
+    fclose(fpDst);
+    
+  }
+
+  fclose(fp);
+  free(pInfo);
+
+  printf("Unzip success\n");
+  return 0;
+}
---
+++ ZipInCode/ZipInCode.cpp
@@ -0,0 +1,95 @@
+﻿// ZipInCode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <tchar.h>
+#include <stdlib.h>
+
+struct ZipData {
+  char filename[256];
+  int filesize;
+};
+
+int getFileSize(char* filename);
+
+int _tmain(int argc, _TCHAR* argv[]) {
+  // file count
+  int num = argc - 1;
+
+  // Prepare the info of file
+
+  // struct ZipData info[num] = {0};
+
+  struct ZipData* pInfo = (struct ZipData*)malloc(num * sizeof(struct ZipData));
+
+  char compressedFileName[256];
+  printf("Enter the name for the compressed output file (without extension): ");
+  std::cin.getline(compressedFileName, 256);
+  strcat_s(compressedFileName, ".AAfile");
+
+  for (int i = 0; i < num; i++) {
+    // get size of file, send file name and file size to the struct
+    pInfo[i].filesize = getFileSize(argv[i + 1]);
+
+    strcpy_s(pInfo[i].filename, 256, argv[i + 1]);
+
+    printf("i:%d File Name: %s File Size: %d\n", i, pInfo[i].filename,
+           pInfo[i].filesize);
+  }
+
+  // Create a new file
+  FILE* fp;
+  if (fopen_s(&fp, compressedFileName, "wb") != 0) {
+    fprintf(stderr, "Failed to create output file: Compressed.AAfile\n");
+    free(pInfo);
+    return -1;
+  }
+  if (NULL == fp) return -1;
+  // Write the count of file
+  fwrite(&num, 1, 4, fp);
+  // Write file information in
+  fwrite(pInfo, 1, num * sizeof(struct ZipData), fp);
+  // For loop read the information and write in
+
+  FILE* fpSrc;
+  int r;
+  char buff[1024];
+
+  for (int i = 0; i < num; i++) {
+    if (fopen_s(&fpSrc, pInfo[i].filename, "rb") != 0) {
+      fprintf(stderr, "Failed to open source file: %s\n", pInfo[i].filename);
+      continue;
+    }
+
+
+    while (1) {
+      r = fread_s(buff, sizeof(buff), 1, 1024, fpSrc);
+      if (r > 0) {
+        fwrite(buff, 1, r, fp);
+      } else {
+        break;
+      }
+    } 
+    fclose(fpSrc);
+  }
+  while (1);
+  return 0;
+}
+
+int getFileSize(char* filename){ 
+    FILE* fp;
+  fopen_s(&fp, filename, "r");
+  if (NULL == fp) return -1;
+
+  //file content pointer to the end
+  fseek(fp, 0, SEEK_END);
+
+  //file content pointer to the file size
+  int size = ftell(fp);
+
+  //file content pointer to the beginning
+  fseek(fp, 0, SEEK_SET);
+  fclose(fp);
+
+  return size;
+}
